0a4c3fc8e8d2c12834f3066ca95d67955370c520
Atul <atulrshu@yahoo.com>
2023-08-15 17:52:49 +0530

    Created maven Eclipse

---
+++ helloWorldMavenEclipse/src/main/java/com/caveofprogramming/maven/helloWorldMavenEclipse/App.java
@@ -0,0 +1,13 @@
+package com.caveofprogramming.maven.helloWorldMavenEclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
